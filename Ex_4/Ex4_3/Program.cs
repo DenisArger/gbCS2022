76d244946643a7bea6b81062ae4c4c7168469ec9
DenisArger <den.arger@gmail.com>
2022-08-09 18:32:01 +0300

    Ex_4_3 done

---
+++ Ex_4/Ex4_3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace ConsoleApp
+{
+    class Program
+    {
+        /*Задача 29: Напишите программу, которая задаёт массив 
+        из 8 случайных целых чисел и выводит отсортированный по модулю массив.
+        */
+        static void Main(string[] arg)
+        {
+            int size = 8;
+            int[] array = new int[size];
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(-100, 100);
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+
+
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (Math.Abs(array[j]) > Math.Abs(array[j + 1]))
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
