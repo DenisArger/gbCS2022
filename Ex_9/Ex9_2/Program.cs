166c35aeddb3d4dbf9a3526168e4938b05cd37c1
DenisArger <den.arger@gmail.com>
2022-08-23 11:39:43 +0300

    Ex9_2 done

---
+++ Ex_9/Ex9_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace ConsoleApp
+{
+    class Program
+    {
+        /*Задача 66: Задайте значения M и N. 
+        Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+        */
+        static void Main(string[] arg)
+        {
+            Console.WriteLine("Введите значение M");
+            int numberM = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение N");
+            int numberN = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("");
+            Console.WriteLine("Cумма натуральных чисел:");
+            int sum = 0;
+            sum = sumNumber(numberM, numberN, sum);
+            Console.WriteLine($"{sum}");
+
+            int sumNumber(int numberM, int numberN, int sum)
+            {
+                if (numberM > numberN)
+                    return sum;
+                sum += numberM;
+                numberM++;
+                return sumNumber(numberM, numberN, sum);
+            }
+
+        }
+    }
+}
